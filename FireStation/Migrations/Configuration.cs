54075a32703f6ad3cb8d78412177b6a0405ebde1
sajjad sadeghi <sadeghi.sajjad1996@outlook.com>
2021-06-27 09:18:17 +0430

    sajjad

--- FireStation/Migrations/Configuration.cs
+++ FireStation/Migrations/Configuration.cs
@@ -14,10 +14,25 @@
 
         protected override void Seed(FireStation.Models.Context context)
         {
-            //  This method will be called after migrating to the latest version.
+            #region states
+            context.tbl_State.AddOrUpdate(x => x.StateId,
+                    new Models.tbl_State() { StateId = 1, StateName = "ایستگاه ۲۲ بهمن", StateAdress = "خیابان 22 بهمنرنبش چهار راه شهربانی", StateTel = "02312345678" },
+                    new Models.tbl_State() { StateId = 2, StateName = "ایستگاه ولایت", StateAdress = "میدان ولایت", StateTel = "02312345678" },
+                    new Models.tbl_State() { StateId = 3, StateName = "ایستگاه بهارستان", StateAdress = "بهارستان", StateTel = "02312345678" },
+                    new Models.tbl_State() { StateId = 4, StateName = "ایستگاه دادگستری", StateAdress = "چهار اره دادگستری", StateTel = "02312345678" },
+                    new Models.tbl_State() { StateId = 5, StateName = "ستاد فرماندهی", StateAdress = "چهار اره دادگستری", StateTel = "02312345678" }
+                    );
+            #endregion
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+            #region defultuser
+            context.tbl_Employee.AddOrUpdate(x => x.EmployeeId,
+                      new Models.tbl_Employee() { EmployeeId = 1, EmployeeName = "علیرضا", EmployeeLastName = "علی بیک", EmployeeAdress = "دادگستری", EmployeeBirthdate = DateTime.Now, EmployeeCode = "9906705", EmployeeMCode = "4581234567", EmployeeFName = "علی", EmployeeWork = "اتشنشان", EmployeePhone = "09121234567", EmployeePicUrl = "/new", EmployeeDateRegistered = DateTime.Now, EmployeeSex = true ,StateId=5}
+                      );
+            context.tbl_User.AddOrUpdate(x => x.UserId,
+                      new Models.tbl_User() { EmployeeId = 1, UserId = 1, UserUserName = "Alireza", UserPassword = "Admin@8520", UserRole = "SUPERADMIN" }
+                      );
+            #endregion
+
         }
     }
 }
